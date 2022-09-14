4ee72593fc95347f3653e4317c6853fdbac132c1
usernameBojan <bojan_1111@live.com>
2022-09-14 21:44:52 +0200

    minimal change to email styling

--- Loto3000.Application/Utilities/EmailContents.cs
+++ Loto3000.Application/Utilities/EmailContents.cs
@@ -6,12 +6,12 @@
         public const string ForgotPasswordSubject = "Loto3000 Password Recovery";
         public static string RegisterBody(string name, string code)
         {
-            string body = $"<p>Hello {name}.</p> </p>Thank you for registering and welcome to Loto3000.</p> <p>Your verification code is {code}</p>";
+            string body = $"<p>Hello {name}.</p> </p>Thank you for registering and welcome to Loto3000.</p> <p>Your verification code is <b>{code}</b></p>";
             return body;
         }
         public static string ForgotPasswordBody(string code)
         {
-            string body = $"<p>Use {code} as your password recovery code.</p>";
+            string body = $"<p>Use <b>{code}</b> as your password recovery code.</p>";
             return body;
         }
     }
